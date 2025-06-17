2c3d4e36d2bbe071726ee87e7d7b82a5bed6fcd0
AngotB <angot0101@gmail.com>
2025-06-17 13:53:27 -0700

    completed lab 2

---
+++ hackthehood-hustleOUSD/lab2.py
@@ -0,0 +1,24 @@
+#step 1
+
+name = "Angel Barrera"
+
+#step 2
+
+nickname = "Angel"
+
+#step 3 
+
+age = 15
+
+#step 4
+
+used_python = True 
+
+#step 5
+
+hobbies = ["videogame collecting", "electronics repair",  "guitar"]
+
+#step 6
+
+favorite_things = {"movie": "Toy Story", "videogame": "Minecraft", "color": "red", "animal": "cat"}
+
